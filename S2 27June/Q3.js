c943eb460feee71a15b66e4a94bfc16a27a2f3ea
soniv ahmad <sonivahmad@gmail.com>
2022-06-28 22:03:33 +0530

    push

---
+++ S2 27June/Q3.js
@@ -0,0 +1,9 @@
+// Take a number and find its cube.
+function cube(a) {
+    // Write your code here
+    let x = a * a * a;
+    return x;
+    // make sure you return the output
+}
+console.log(cube(3));
+exports.cube = cube 
